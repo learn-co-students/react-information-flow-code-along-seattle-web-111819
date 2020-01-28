c67fc9ed786d1e2f324fea4e7d0b1cb2da5e64dd
4myc <missch3n@gmail.com>
2020-01-27 17:52:38 -0800

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,26 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
+  }
+
+  // run this changeColor() method when either Child component is clicked. 
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        {/* pass in changeColor as a prop to both Child components */}
+        {/* Since the data that represents the color of the two Child components lives in Parent, we should pass in childrenColor as props: */}
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
